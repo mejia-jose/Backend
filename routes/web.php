7a75d6268c91afc9a4358128af382e5b57217355
jmejia <jmejia@nexura.com>
2024-06-21 19:38:10 -0500

    Creación del webhok para dialogflow

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DialogFlowController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::post('/dialogflow-webhook', [DialogFlowController::class, 'webhook'])->name('dialogflow-webhook');
+
 
 Route::get('/', function () {
     return view('welcome');
